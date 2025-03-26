703ff55b871f52e1b93500b225d4b7be5c68db13
mamadrepo <nywzhq@gmail.com>
2025-03-26 22:53:55 +0330

    c variables

---
+++ c/jaf/s1/c_variable.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int x, y = 5;
+
+	int one, two;
+	one = 10;
+	two = 11;
+
+	int a, b, c;
+	a = b = c = 0;
+
+
+	int test;
+	scanf("%d", &test);
+
+	return 0;
+}
