3f81a5d452dfde6838c2e95c79882e21bc4b578f
mamadrepo <nywzhq@gmail.com>
2025-03-29 15:24:16 +0330

    input/output character

---
+++ c/jaf/s2/char_input_output.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	char ch_one = 0;
+
+	printf("Enter character: ");
+	ch_one = getchar();
+
+	printf("Entered character:\n");
+	putchar(ch_one);
+	putchar('\n');
+
+
+	return 0;
+}
