7770831db81a3934a64839ee6d42c3e6ac06f2b1
mamadrepo <nywzhq@gmail.com>
2025-03-31 09:28:13 +0330

    variable in python, and show its types

---
+++ python/py4every/s2/variable.py
@@ -0,0 +1,8 @@
+message = "A message for testing"
+num_one = 3
+num_two = 3.1
+
+
+print("message -->", message, "\tType of message: ", type(message));
+print("num_one -->", num_one, "\tType of num_one: ", type(num_one));
+print("num_two -->", num_two, "\tType of num_two: ", type(num_two));
