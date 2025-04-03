c0990e267d48d97d8b82401aafc983568b8884db
mamadrepo <nywzhq@gmail.com>
2025-04-03 20:49:50 +0330

    Calculating hourly wage

---
+++ python/py4every/s3/rate_hour.py
@@ -0,0 +1,21 @@
+try:
+    hr = int(input('Enter hour: '))
+except:
+    print('Only enter integer!')
+    hr = -1
+
+try:
+    rt = float(input('Enter rate per hour: '))
+except:
+    print('Wrong input!')
+    rt = -1
+
+if hr > 0 and rt > 0:
+    if hr <= 40:
+        print("Pay --> ", round(hr * rt, 2))
+    else:
+        pay = 40 * rt;
+        pay += (hr % 40) * (rt * 1.5)
+        print('Pay --> ', round(pay, 2))
+else:
+    print("You entered wrong type")
